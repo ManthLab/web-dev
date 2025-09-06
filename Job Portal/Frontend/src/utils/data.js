274fefda5129e2e024c4ddc22d50809834eaaa50
ManthLab <pawarmanthan2105@gmail.com>
2025-09-06 18:22:47 +0530

    Delete Job Portal directory

--- Job Portal/Frontend/src/utils/data.js
+++
@@ -1,6 +0,0 @@
-
-export const USER_API_ENDPOINT = "http://localhost:5033/api/users";
-
-export const JOB_API_ENDPOINT = "http://localhost:5033/api/job";
-
-export const APPLICATION_API_ENDPOINT = "http://localhost:5033/api/application";
